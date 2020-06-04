e29ce92387327956cbbd3fa7da593de622aa4444
ahelja@alpenite.com <ahelja@alpenite.com>
2020-06-04 21:02:57 +0200

    Create make-alias.sh

---
+++ make-alias.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+# chmod a+x
+ln -s .zprofile $HOME/.zprofile
+ln -s .zsh_aliases $HOME/.zsh_aliases
+ln -s .vimrc $HOME/.vimrc
+ln -s material-monokai.vim $HOME/.vim/colors/material-monokai.vim
